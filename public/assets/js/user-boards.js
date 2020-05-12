69144a0eabf8d5429ac7312473071d2ea811acbb
Austin Atwood <austinatwoodjs30@gmail.com>
2020-05-12 21:48:21 +0000

    renderboards

--- public/assets/js/user-boards.js
+++ public/assets/js/user-boards.js
@@ -2,6 +2,7 @@ const $logoutButton = $('#logout');
 const $newBoardButton = $('#new-board');
 const $boardNameInput = $('#board-name')
 const $saveBoardButton = $('#save-board');
+const $boardsContainer = $('.boards');
 
 let user;
 
@@ -14,15 +15,48 @@ function init() {
         location.replace('/');
         return;
     }
-
     $('.welcome h1').text('Welcome ' + user.email + '!');
+    getUserBoards();
+}
+
+function getUserBoards() {
+    $.ajax({
+        url: `/api/users/${user.id}`,
+        method: 'GET'
+    }).then(function(data) {
+        renderBoards(data.boards);
+    });
+}
+
+function renderBoards(boards) {
+    $boardsContainer.empty();
+    let $boardTiles = boards.map(function(board) {
+        let $boardTile = $('<a class="board-tile">')
+            .attr('href', `/boards/${board.id}`)
+            .text(board.name);
+        return $boardTile;
+    });
+
+    $boardsContainer.append($boardTiles);
 }
 
 function handleBoardCreate(event) {
     event.preventDefault();
     let boardName = $boardNameInput.val().trim();
     $boardNameInput.val('');
-    console.log(boardName);
+    if (!boardName) {
+        return;
+    }
+    $.ajax({
+        url: '/api/boards',
+        data: {
+            name: boardName
+        },
+        method: 'POST'
+    }).then(function(data) {
+        getUserBoards();
+        MicroModal.close('create-board');
+    });
 }
 
 function handleLogout() {
